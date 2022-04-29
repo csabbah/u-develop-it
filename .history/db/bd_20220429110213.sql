2dd31a47442cc3ed2a3646fe8db71e36a7adf12a
Csabbah <csabbah@ryerson.ca>
2022-04-29 11:27:42 -0400

    Added SQL files and included code to create the database and populate it with data

---
+++ .history/db/bd_20220429110213.sql
@@ -0,0 +1,4 @@
+-- Drop the data base ONLY if it exists
+DROP DATABASE IF EXISTS election;
+CREATE DATABASE election;
+USE election;
---
+++ db/db.sql
@@ -0,0 +1,5 @@
+-- Drop the data base ONLY if it exists
+DROP DATABASE IF EXISTS election;
+-- Other wise create the 'election' database
+CREATE DATABASE election;
+USE election;
