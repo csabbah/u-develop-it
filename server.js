de989ce31ba4cd5aa71edd108b59d9b8c2609227
Csabbah <csabbah@ryerson.ca>
2022-04-30 16:36:27 -0400

    Successfully added the connection to the database in the express server (server.js)

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Spartan123!',
+    database: 'election',
+  },
+  console.log('Connected to the election database.')
+);
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
