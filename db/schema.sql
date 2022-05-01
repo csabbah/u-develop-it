b455f28508bf4c4d1923a56de9652da0a94c7f6a
Csabbah <csabbah@ryerson.ca>
2022-05-01 10:06:41 -0400

    Added new 'voters' table and populated it with data

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS voters;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 
@@ -14,4 +15,13 @@ CREATE TABLE candidates (
   party_id INTEGER,
   industry_connected BOOLEAN NOT NULL,
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
+-- When it comes to voters, there is no relation with candidates or parties so we'll create a seperate table for this
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP -- This will return WHEN the voters were added
 );
