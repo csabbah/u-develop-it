543188a85ef2bf761323afe4fb8bc5bef1f2e170
Csabbah <csabbah@ryerson.ca>
2022-05-01 15:32:34 -0400

    Added new voters table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
-DROP TABLE IF EXISTS voters;
+DROP TABLE IF EXISTS votes;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -24,4 +25,18 @@ CREATE TABLE voters (
   last_name VARCHAR(30) NOT NULL,
   email VARCHAR(50) NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP -- This will return WHEN the voters were added
+);
+
+CREATE TABLE votes (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  voter_id INTEGER NOT NULL,
+  candidate_id INTEGER NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT uc_voter UNIQUE (voter_id),-- This signifies that the values inserted into the voter_id field must be unique. 
+  -- For example, whoever has a voter_id of 1 can only appear in this table once.
+  -- INSERT INTO votes (voter_id, candidate_id) VALUES(2, 1);
+ --INSERT INTO votes (voter_id, candidate_id) VALUES(2, 2);
+
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE, -- CASCADE > deleting the reference key will also delete the entire row from this table
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
