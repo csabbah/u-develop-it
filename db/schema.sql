32fccb6cb66836262e13f120c7c0bb94817d5d9e
Csabbah <csabbah@ryerson.ca>
2022-05-01 15:45:46 -0400

    Added new vote routes

--- db/schema.sql
+++ db/schema.sql
@@ -33,10 +33,14 @@ CREATE TABLE votes (
   candidate_id INTEGER NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT uc_voter UNIQUE (voter_id),-- This signifies that the values inserted into the voter_id field must be unique. 
-  -- For example, whoever has a voter_id of 1 can only appear in this table once.
+  -- For example, whoever has a voter_id of 1 can only appear in this table once. See below, we excute the following commands to input the voters
   -- INSERT INTO votes (voter_id, candidate_id) VALUES(2, 1);
- --INSERT INTO votes (voter_id, candidate_id) VALUES(2, 2);
-
+  -- INSERT INTO votes (voter_id, candidate_id) VALUES(2, 2); < This line would return and errors as the a voter_id of 1 already voted
   CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE, -- CASCADE > deleting the reference key will also delete the entire row from this table
   CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
+
+-- And as a test of the above CASCADE commands, if we run this in terminal: 
+-- DELETE FROM voters WHERE id = 2;
+-- SELECT * FROM votes;
+-- Rather than the NULL command that makes the value NULL, the full row will be deleted from the votes table if the voter is deleted
